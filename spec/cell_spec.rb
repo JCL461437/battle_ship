d8c7fca837f1d102b9abfe54a022dfd73e31d8de
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-04 20:22:32 -0600

    Add tests for the ship, empty?, and place_ship methods in the cell_spec.rb

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -11,9 +11,39 @@ RSpec.describe Cell do
   end
   
     describe '#initialize' do
-        
         it 'can initialize' do
             expect(@cell).to be_an_instance_of(Cell)
         end
+        
+        it 'can return a coordinate value' do
+            expect(@cell.coordinate).to eq("B4")
+        end
+        
     end
+
+    describe '#ship' do
+        it 'can tell if a ship is present' do
+            expect(@cell.ship).to eq(nil)
+        end
+    end
+
+    describe '#empty?' do
+        it 'can tell if a cell is empty' do
+            expect(@cell.empty?).to be true
+        end
+    end
+
+    describe '#place_ship?' do
+    it 'can place a ship' do
+        cruiser = Ship.new("Cruiser", 3)
+        
+        expect(@cell.empty?).to be true
+
+        @cell.place_ship(cruiser)
+
+        expect(@cell.ship).to eq(cruiser)
+
+        expect(@cell.empty?).to be false
+    end
+end
 end
