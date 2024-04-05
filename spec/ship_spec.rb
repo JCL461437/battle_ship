1a812a513512664036afad04fe048ec0df2ce66b
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-04 19:42:13 -0600

    Add tests for the Ship class in the ship_spec.rb file

---
+++ spec/ship_spec.rb
@@ -0,0 +1,53 @@
+require './lib/ship'
+
+RSpec.configure do |config|
+      config.formatter = :documentation
+end
+
+RSpec.describe Ship do
+  before(:each) do
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+  
+    describe '#initialize' do
+        
+        it 'can initialize' do
+            expect(@crusier).to be_an_instance_of(Ship)
+        end
+
+        it 'has a name' do
+            expect(@cruiser.name).to eq("Cruiser")
+        end
+
+        it 'has a length' do
+            expect(@cruiser.length).to eq(3)
+        end
+
+        it 'has health' do
+            expect(@cruiser.health).to eq(3)
+        end
+
+        it 'is possible to sink' do
+            expect(@cruiser.sunk?).to be false
+        end
+
+        it 'can be hit and sunk' do
+            expect(@cruiser.health).to eq(3)
+            expect(@cruiser.sunk?).to be false
+            @cruiser.hit
+            
+            expect(@cruiser.health ).to eq(2)
+            expect(@cruiser.sunk?).to be false
+            @cruiser.hit
+            
+            expect(@cruiser.health ).to eq(1)
+            expect(@cruiser.sunk?).to be false
+            @cruiser.hit
+            
+            expect(@cruiser.health ).to eq(0)
+            expect(@cruiser.sunk?).to be true
+        end
+
+    end
+
+end
