01501e8dbc947a7ee5feb9406b690fd278c40071
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-06 10:21:54 -0600

    Add test to see if a placement is valid for Board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -62,6 +62,16 @@ RSpec.describe Board do
             expect(@board.valid_placement?(submarine, ["C2","D3"])).to be false
          
         end
+        
+        it 'can decide if a placement is valid' do
+            cruiser = Ship.new("Cruiser", 3)
+            submarine = Ship.new("Submarine", 2)
+        
+            expect(@board.valid_placement?(submarine, ["A1", "A2"])).to be true
+            expect(@board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be true
+         
+        end
+
     end
     
 end
