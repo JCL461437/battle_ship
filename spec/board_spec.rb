7e885e7b1355952b8642669755df20d5c0adbaea
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-06 10:03:12 -0600

    Add refactored initialize method test and add validating_coordinate? method test (paired)

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -16,8 +16,20 @@ RSpec.describe Board do
             expect(@board).to be_an_instance_of(Board)
         end
         
+        it 'can collect all cells' do
+            expect(@board.cells).to eq(all_cells)
+        end
+        
+    end
+
+    describe '#validating_coordinate?' do
         it 'can ' do
             expect(@board.cells).to eq(all_cells)
+
+            expect(@board.valid_coordinate?("A1")).to be true
+            expect(@board.valid_coordinate?("D4")).to be true
+            expect(@board.valid_coordinate?("A5")).to be false
+            expect(@board.valid_coordinate?("A22")).to be false
         end
         
     end
