5a5a2f16e0ba44b3422194e1e09629c2c6764c20
Austin Kenny <austinkennywork@gmail.com>
2024-04-09 19:34:21 -0400

    AK Async work for game class

--- lib/game.rb
+++ lib/game.rb
@@ -4,23 +4,15 @@ require './lib/board'
 require 'pry'
 
 class Game
-    attr_reader 
+    attr_reader :board
 
-    def initialize
+    def initialize(board)
+        @board
     end
 
-    def main_menu
-        puts "Welcome to BATTLESHIP"
-        puts "Enter [p] to play. Enter [q] to quit." 
-        answer = gets 
-        if answer == "p"
-            
-        elsif answer == "q"
-
-        else 
-            puts "Invalid input. Please select again."
-        end
-
+    def random_ships
+        
+    end
 
 
 
